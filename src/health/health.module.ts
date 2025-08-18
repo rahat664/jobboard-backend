bfba798c0178db04d2e7b12a0700922279b5718d
r.kabir <r.kabir@shamolima.com>
2025-08-18 13:10:59 +0600

    feat(health): add /api/health endpoint

---
+++ src/health/health.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { HealthController } from './controller/health.controller';
+
+@Module({ controllers: [HealthController] })
+export class HealthModule {}
